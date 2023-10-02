5b78a9f16968391ebda5076940b1b76f2002e9f8
Natalia Pankowska <npankowska@gmail.com>
2023-10-02 18:32:09 +0200

    search bar step one

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,16 +4,20 @@ import './SearchBar.css'
 
 function SearchBar() {
 const [searchInput,  setSearchInput] = useState('');
+function handleType(e){
+    setSearchInput(e.target.value);
+}
 return(
     
     
        <div className='search'>
         <div className='searchInput'>
-        <input id="search" type="text" placeholder="search"/>
-        </div>
+        <input id="search" type="text" placeholder="search" onChange={handleType} value={searchInput}/>
         <div className='searchIcon'>
             <SearchIcon />
         </div>
+        </div>
+      
       
         </div>
        
